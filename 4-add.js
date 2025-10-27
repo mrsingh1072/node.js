9f400ef3a62f1d7eba8ecb54b31c33efa039739d
Saurabh Kumar <saurabhkumar08843@gmail.com>
2025-10-27 17:32:42 +0530

    Removed 1-node-tutorial folder

--- 4-add.js
+++
@@ -1,2 +0,0 @@
-const adding=require('./3-modules.js');
-console.log("Sum of two number is:- ",adding.add(2,4));
--- 5-sub.js
+++
@@ -1,2 +0,0 @@
-const subtracting=require('./3-modules.js');
-console.log("subtraction of two number is:-",subtracting.subtract(2,4));
